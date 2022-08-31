722aba63281605e58296933a3ace4f3d491da71e
Kartik <101096226+kartikchauhan13@users.noreply.github.com>
2022-08-31 15:09:13 +0530

    Update settings.py

--- myWebsite/myWebsite/settings.py
+++ myWebsite/myWebsite/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-
+#secret key removed
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
